76bdc7970c285d6e2844354f9cade9acffc84bca
Your Name <thomas_hmm@LAPTOP-VSPB1DQN>
2023-09-18 16:50:58 +0200

    BinnedData

---
+++ chapters/BinnedData/DataAnalysisProject/Source/Analysis/DataAnalyzer.cs
@@ -0,0 +1,82 @@
+using System;
+using static System.Console;
+using System.Collections.Generic;
+
+namespace DataAnalysisProject.Analysis
+{
+    public class DataAnalyzer
+    {
+        // Fields, properties, or constructor if needed
+
+        // Example method for analyzing data
+
+        public static void Test(){
+            WriteLine("Working");
+        }
+
+        // public AnalysisResult AnalyzeData(List<double> data)
+        // {
+        //     // Implement the logic to analyze the data using different numerical methods
+        //     // Replace double with the actual data type you are using
+        //     // You can use the methods from the NumericalMethods folder or implement new ones here
+
+        //     // Example: Calculate the mean of the data
+        //     double mean = CalculateMean(data);
+
+        //     // Example: Calculate the standard deviation of the data
+        //     double standardDeviation = CalculateStandardDeviation(data);
+
+        //     // Example: Perform additional data analysis tasks
+
+        //     // Return the results as an instance of AnalysisResult
+        //     return new AnalysisResult
+        //     {
+        //         Mean = mean,
+        //         StandardDeviation = standardDeviation,
+        //         // Add more properties as needed
+        //     };
+        // }
+
+        // // Example method to calculate the mean of a list of double values
+        // private double CalculateMean(List<double> data)
+        // {
+        //     // Implement the logic to calculate the mean
+        //     // Replace double with the actual data type you are using
+        //     double sum = 0;
+        //     foreach (var value in data)
+        //     {
+        //         sum += value;
+        //     }
+        //     return sum / data.Count;
+        // }
+
+        // // Example method to calculate the standard deviation of a list of double values
+        // private double CalculateStandardDeviation(List<double> data)
+        // {
+        //     // Implement the logic to calculate the standard deviation
+        //     // Replace double with the actual data type you are using
+        //     double mean = CalculateMean(data);
+        //     double sumSquaredDifferences = 0;
+
+        //     foreach (var value in data)
+        //     {
+        //         double difference = value - mean;
+        //         sumSquaredDifferences += difference * difference;
+        //     }
+
+        //     return Math.Sqrt(sumSquaredDifferences / data.Count);
+        // }
+
+        // Add more methods for additional data analysis tasks as needed
+    }
+
+    // Replace double with the actual data type you are using
+    // Define the AnalysisResult class or structure to hold the analysis results
+    // Example:
+    // public class AnalysisResult
+    // {
+    //     public double Mean { get; set; }
+    //     public double StandardDeviation { get; set; }
+    //     // Add more properties as needed
+    // }
+}
---
+++ chapters/BinnedData/DataAnalysisProject/Source/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+namespace DataAnalysisProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data Analysis Project");
+        }
+
+}
+}
